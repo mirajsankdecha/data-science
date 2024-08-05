2efe9ca6f01d589f6f49d342d26feb52d2aa7f38
mirajsankdecha <mirajsankdecha@gmail.com>
2024-08-05 12:03:27 +0530

    Type Function

--- Portfolio.py
+++ Portfolio.py
@@ -1,3 +1,5 @@
+# Portfolio Program 
+
 fn = input("Enter the  Your First Name :")
 ln = input("Enter the Your Last Name :")
 dad = input("Enter the  Your Fathers Name :")
@@ -8,4 +10,17 @@ deg = input("Enter the Your Degree :")
 yr = input("Enter the Your Year of Passing :")
 cgpa = input("Enter the Your CGPA :")
 print("Your Portfolio :",fn,ln,dad,mom,dob,clg,deg,yr,cgpa)
+
+"""Hellow World
+dsfdsf
+dsfdsf"""
+
+# Type Function
+
+# counter = 100
+# miles = 1000.0
+# name = "Mahadev"
+# print(type(counter))
+# print(type(miles))
+# print(type(name))
 
