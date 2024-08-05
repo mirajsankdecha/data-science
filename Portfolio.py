9a9806898805f974c08400e97700ef540690435a
mirajsankdecha <mirajsankdecha@gmail.com>
2024-08-05 11:00:52 +0530

    Portfolio Program created

---
+++ Portfolio.py
@@ -0,0 +1,11 @@
+fn = input("Enter the  Your First Name :")
+ln = input("Enter the Your Last Name :")
+dad = input("Enter the  Your Fathers Name :")
+mom = input("Enter the Your Mothers Name :")
+dob = input("Enter the Your Date of Birth :")
+clg = input("Enter the Your College Name :")
+deg = input("Enter the Your Degree :")
+yr = input("Enter the Your Year of Passing :")
+cgpa = input("Enter the Your CGPA :")
+print("Your Portfolio :",fn,ln,dad,mom,dob,clg,deg,yr,cgpa)
+
