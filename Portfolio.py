1d50456bd077dcbda7e849d5936b62a961912a0a
mirajsankdecha <mirajsankdecha@gmail.com>
2024-08-05 12:08:29 +0530

    Type function Update

--- Portfolio.py
+++ Portfolio.py
@@ -1,15 +1,15 @@
 # Portfolio Program 
 
-fn = input("Enter the  Your First Name :")
-ln = input("Enter the Your Last Name :")
-dad = input("Enter the  Your Fathers Name :")
-mom = input("Enter the Your Mothers Name :")
-dob = input("Enter the Your Date of Birth :")
-clg = input("Enter the Your College Name :")
-deg = input("Enter the Your Degree :")
-yr = input("Enter the Your Year of Passing :")
-cgpa = input("Enter the Your CGPA :")
-print("Your Portfolio :",fn,ln,dad,mom,dob,clg,deg,yr,cgpa)
+# fn = input("Enter the  Your First Name :")
+# ln = input("Enter the Your Last Name :")
+# dad = input("Enter the  Your Fathers Name :")
+# mom = input("Enter the Your Mothers Name :")
+# dob = input("Enter the Your Date of Birth :")
+# clg = input("Enter the Your College Name :")
+# deg = input("Enter the Your Degree :")
+# yr = input("Enter the Your Year of Passing :")
+# cgpa = input("Enter the Your CGPA :")
+# print("Your Portfolio :",fn,ln,dad,mom,dob,clg,deg,yr,cgpa)
 
 """Hellow World
 dsfdsf
@@ -20,7 +20,11 @@ dsfdsf"""
 # counter = 100
 # miles = 1000.0
 # name = "Mahadev"
+# my_bool = True
+# non = None
 # print(type(counter))
 # print(type(miles))
 # print(type(name))
+# print(type(my_bool)) 
+# print(type(None)) 
 
