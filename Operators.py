2b160314faf806b3944ed88002b4fe13de80e992
mirajsankdecha <mirajsankdecha@gmail.com>
2024-08-08 11:22:10 +0530

    Arithmetic Operator

---
+++ Operators.py
@@ -0,0 +1,20 @@
+# Arithmetic Opertors in Python
+
+num_1 = int(input("Enter the First Number : "))
+numb_2 = int(input("Enter the Second Number : "))
+
+add = num_1 + numb_2
+sub = num_1 - numb_2
+mul = num_1 * numb_2
+div = num_1 / numb_2
+f_div = num_1 // numb_2
+mod = num_1 % numb_2
+exp = num_1 ** numb_2
+
+print("Addition of Two Numbers :",add)
+print("Subtraction of Two Numbers :",sub)
+print("Multiplication of Two Numbers :",mul)
+print("Division of Two Numbers :",div)
+print("Floor Division of Two Numbers :",f_div)
+print("Modulus of Two Numbers :",mod)
+print("Exponential of Two Numbers :",exp)
--- typecasting.py
+++ typecasting.py
@@ -1,4 +1,5 @@
 num = int(input("Enter the Number : "))
 print("The Number you enter is :",type(num))
 num = float()
-print("The Number you enter is :",type(num))
+print(" Now the Number you enter is :",type(num))
+
