2ddaa7ec438603f30c8565b6f1bf5451348100b0
mirajsankdecha <mirajsankdecha@gmail.com>
2024-08-09 15:42:38 +0530

    KBC Question Create

---
+++ KBC.py
@@ -0,0 +1,62 @@
+q1 = "Who developed Python Programming Language?"
+q1o1 = "Wick van Rossum"
+q1o2 = "Rasmus Lerdorf"
+q1o3 = "Guido van Rossum"
+q1o4 = "Niene Stom"
+
+q2 = "Which type of Programming does Python support?"
+q2o1 = "object-oriented programming"
+q2o2 = "structured programming"
+q2o3 = "functional programming"
+q2o4 = " all of the mentioned"
+
+q3 = "Is Python case sensitive when dealing with identifiers?"
+q3o1 = "no"
+q3o2 = "yes"
+q3o3 = "machine dependent"
+q3o4 = "none of the mentioned   "
+
+q4 = "Which of the following is the correct extension of the Python file?"
+q4o1 = ".python"
+q4o2 = ".pl"
+q4o3 = ".py"
+q4o4 = ".p"
+
+q5 = "Is Python code compiled or interpreted?"
+q5o1 = "Python code is both compiled and interpreted"
+q5o2 = "Rasmus Lerdorf"
+q5o3 = "Guido van Rossum"
+q5o4 = "Niene Stom"
+
+
+q6 = " All keywords in Python are in"
+q6o1 = "Capitalized"
+q6o2 = "lower case"
+q6o3 = "UPPER CASE"
+q6o4 = "None of the mentioned"
+
+q7 = "What will be the value of the following Python expression? 4 + 3 % 5"
+q7o1 = "7"
+q7o2 = "2"
+q7o3 = "4"
+q7o4 = "1"
+
+q8 = "Which of the following is used to define a block of code in Python language?"
+q8o1 = "Indentation"
+q8o2 = "Key"
+q8o3 = "Brackets"
+q8o4 = "All of the mentioned"
+
+q9 = "Which keyword is used for function in Python language?"
+q9o1 = "Function"
+q9o2 = "def"
+q9o3 = "Fun"
+q9o4 = "Define"
+
+q10 = "Which of the following character is used to give single-line comments in Python?"
+q10o1 = "//"
+q10o2 = "#"
+q10o3 = "!"
+q10o4 = "/*"
+
+
