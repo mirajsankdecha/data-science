fa8c16dc10b2d61511eca3d4f3cb6dd4b1446072
mirajsankdecha <mirajsankdecha@gmail.com>
2024-08-09 14:59:29 +0530

    Age Category program created

--- Condition.py
+++ Condition.py
@@ -2,10 +2,21 @@
 
 a  = int(input("Enter Your Age :"))
 
-
 if a >= 18 :
     print("You are Eligible for Voting")
 else :
     print("Sorry, you are not Eligible for Voting")
-    
+
+age = int(input("Enter your Age : ")) 
+
+if age >= 13 and age <= 19 : 
+    print("Your are Teen") 
+elif age >= 2 and age <=4 :
+    print("You are Toddler")
+elif age >= 5 and age <= 12 :
+    print("You are Child")
+elif age >= 20 and age <= 39 :
+    print("You are Adult")          
+else :
+    print("You are Older") 
 
