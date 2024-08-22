099d26703dc2ac93bed5955ee3074e10d1cea0fe
mirajsankdecha <mirajsankdecha@gmail.com>
2024-08-22 11:04:13 +0530

    Neted Range Increment

--- for_loop.py
+++ for_loop.py
@@ -10,5 +10,11 @@ for i in range(5,0,-1) :
     for j in range(i) :
         print("* ",end="")
     print()     
-# Center Diamond
-    
+# Range Increment
+for i in range(1,10,4) :
+    print("* \n")
+# Nested Range Increment
+for i in range(1,6) :
+    for j in range(1,6):
+        print("* \n")
+    print()    
