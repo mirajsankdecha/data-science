6163d1b4a31ac5f438b68215d3683e8fb044bab9
mirajsankdecha <mirajsankdecha@gmail.com>
2024-08-23 11:52:37 +0530

    Create unit 2 program

---
+++ Unit 2/String_indexing.py
@@ -0,0 +1,14 @@
+name = input("Enter your Name : ")
+
+print(type(name)) # Define Type of Variable
+print(len(name))  # Define Length
+
+print(name[0]) # Print "M"
+print(name[1]) # Print "I"
+print(name[2]) # Print "R"
+print(name[3]) # Print "A"
+print(name[4]) # Print "J"
+
+
+for i in range(0,5,1) :
+    print(name[i]) 
