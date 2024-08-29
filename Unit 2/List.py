1d2d71b97835149377ffa378c9b15c5b94f22867
mirajsankdecha <mirajsankdecha@gmail.com>
2024-08-29 12:19:22 +0530

    Start List program

---
+++ Unit 2/List.py
@@ -0,0 +1,15 @@
+
+student = ["Miraj,Kusagra,Nilesh,Shubham,"]
+print(student)
+print(student[0])
+
+student_marks = [90, 80, 70, 60]
+print(student_marks)
+print(student_marks[0])
+
+student_marks.pop(2)
+print(student)
+
+student_marks.append(55)
+print(student_marks)
+
