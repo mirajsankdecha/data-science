642a8f5b871ed960d875ee3a604d1c259bae8954
mirajsankdecha <mirajsankdecha@gmail.com>
2024-08-30 11:59:58 +0530

    List String and Number Functions

--- Unit 2/List.py
+++ Unit 2/List.py
@@ -1,25 +1,38 @@
+# String Operations 
 
-student = ["Miraj,Kusagra,Nilesh,Shubham,"]
-print(student) # This will print the list
-print(type(student)) # This will tell us the type of the variable
-print(student.reverse()) # This will reverse the list
-print(student.sort()) # This will sort the list in ascending order
-print(student.sort(reverse=True)) # This will sort the list in descending order
-print(student.index("Miraj"))   # This will give us the index of 'Miraj' in the list
-print(student[0]) # This will give us the first element in the list
+student = ["Miraj,Kushagra,Nilesh,Shubham,"]
 
-student_marks = [90, 80, 70, 60]
-print(student_marks) 
+print(student)  # This will print the list
+print(type(student))  # This will tell us the type of the variable
+print(student.reverse())  # This will reverse the list
+print(student.sort())  # This will sort the list in ascending order
+print(student.sort(reverse=True))  # This will sort the list in descending order
+print(student.index("Miraj"))  # This will give us the index of 'Miraj' in the list
+print(student[0])  # This will give us the first element in the list
 
-student_marks.pop(2) # This will remove the element at index 2
-print(student)
+nm = "Miraj"
+print("_" + nm[:2] + "_" + nm[2:5])  # This will print 'Mi_raj'
 
-student_marks.append(55) # This will add 55 to the end of the list
+nm = "Mir aj"
+print(nm.replace(" ", ""))  # This will remove all spaces from the string
+
+# Number Operations
+
+student_marks = [10, 20, 30, 40, 50]
+print(student_marks)  # This will print the list of student marks
+
+for j in student_marks:
+    print(j)  # This will print all the elements in the list
+    print(j**2)  # This will print the square of each element
+
+student_marks.pop(2)  # This will remove the element at index 2
 print(student_marks)
 
-nm = "Miraj" 
-print("_" + nm[:2] +  "_" + nm[2:5]   )   # This will print 'Mi_raj'
+student_marks.append(60)  # This will add 60 to the end of the list
+print(student_marks)
 
-nm = " Mir aj"
-print(nm.replace(" ","")) # This will remove all spaces from the string
+new_list = []
 
+for i in student_marks:
+    new_list.append(i**2)  # This will append the square of each element to the new list
+print(new_list)  # This will print the new list
