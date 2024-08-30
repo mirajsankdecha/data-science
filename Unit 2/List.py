e7eb73cf8ebe2c0d7ac72e2e2ad489d8fcf2ceb7
mirajsankdecha <mirajsankdecha@gmail.com>
2024-08-30 10:58:23 +0530

    Create Program for list in python

--- Unit 2/List.py
+++ Unit 2/List.py
@@ -1,15 +1,21 @@
 
 student = ["Miraj,Kusagra,Nilesh,Shubham,"]
-print(student)
-print(student[0])
+print(student) # This will print the list
+print(type(student)) # This will tell us the type of the variable
+print(student.reverse()) # This will reverse the list
+print(student.sort()) # This will sort the list in ascending order
+print(student.sort(reverse=True)) # This will sort the list in descending order
+print(student.index("Miraj"))   # This will give us the index of 'Miraj' in the list
+print(student[0]) # This will give us the first element in the list
 
 student_marks = [90, 80, 70, 60]
-print(student_marks)
-print(student_marks[0])
+print(student_marks) 
 
-student_marks.pop(2)
+student_marks.pop(2) # This will remove the element at index 2
 print(student)
 
-student_marks.append(55)
+student_marks.append(55) # This will add 55 to the end of the list
 print(student_marks)
+
+# %whos
 
