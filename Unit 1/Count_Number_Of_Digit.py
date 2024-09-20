f844c7755e3716b8a879f03321c17e2a616a51b1
mirajsankdecha <mirajsankdecha@gmail.com>
2024-09-20 12:55:32 +0530

    Sum of given number

--- Unit 1/Count_Number_Of_Digit.py
+++ Unit 1/Count_Number_Of_Digit.py
@@ -1,3 +1,4 @@
+# Count the number of digits in a number and sum of digits in a number
 num = int(input("Enter The Number: "))  # Take input from the user
 count = 0  # Initialize count to 0
 
@@ -6,3 +7,13 @@ while num > 0:
     count += 1  # Increase the count by 1
 
 print("Number of digits:", count)
+
+# Sum of digits in a number
+num = int(input("Enter the Number: "))  # Take input from the user
+sum_digits = 0  # Initialize sum to 0
+
+while num > 0:
+    sum_digits += num % 10  # Add the last digit to sum
+    num = num // 10  # Remove the last digit from the number
+
+print("Sum of digits:", sum_digits)
