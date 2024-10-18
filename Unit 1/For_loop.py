2362015b91591bd35efd169b07da9ed807571568
mirajsankdecha <mirajsankdecha@gmail.com>
2024-10-18 05:51:47 +0530

    Update the Code in Unit 2 Programs

--- Unit 1/For_loop.py
+++ Unit 1/For_loop.py
@@ -1,80 +1,80 @@
 # # Pratice Of Asterisk Using For Loop Pattern 
 
-# # Simple
-# for i in range(1,6,1) :
-#     for j in range(1,i+1) :
-#         print("* ",end="")
-#     print()
-# # Reverse
-# for i in range(5,0,-1) :
-#     for j in range(i) :
-#         print("* ",end="")
-#     print()     
-# # Reverse Short Program
-# for i in range(5,0,-1) :
-#     print("* " * i)
-# # Range Increment
-# for i in range(1,10,4) :
-#     print("* \n")
-# # Nested Range Increments
-# for i in range(1,6) :
-#     for j in range(1,6):
-#         print("* \n")
-#     print() 
-# # Square Pattern 
-# for i in range(1,6) :
-#     for j in range(1,6):
-#         print("* ", end=" ")
-#     print()       
+# Simple
+for i in range(1,6,1) :
+    for j in range(1,i+1) :
+        print("* ",end="")
+    print()
+# Reverse
+for i in range(5,0,-1) :
+    for j in range(i) :
+        print("* ",end="")
+    print()     
+# Reverse Short Program
+for i in range(5,0,-1) :
+    print("* " * i)
+# Range Increment
+for i in range(1,10,4) :
+    print("* \n")
+# Nested Range Increments
+for i in range(1,6) :
+    for j in range(1,6):
+        print("* \n")
+    print() 
+# Square Pattern 
+for i in range(1,6) :
+    for j in range(1,6):
+        print("* ", end=" ")
+    print()       
 
-# # Number of rows for the triangle
-# num_rows = int(input("Enter the number of rows for the triangle: "))
+# Number of rows for the triangle
+num_rows = int(input("Enter the number of rows for the triangle: "))
 
-# # Print the triangle
-# for i in range(num_rows):
-#     # Print leading spaces
-#     print(' ' * (num_rows - i - 1), end='')
+# Print the triangle
+for i in range(num_rows):
+    # Print leading spaces
+    print(' ' * (num_rows - i - 1), end='')
     
-#     # Print the border of the triangle
-#     for j in range(2 * i + 1):
-#         if j == 0 or j == 2 * i or i == num_rows - 1:
-#             print('*', end='')
-#         else:
-#             print(' ', end='')
+    # Print the border of the triangle
+    for j in range(2 * i + 1):
+        if j == 0 or j == 2 * i or i == num_rows - 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
     
-#     print()  # Move to the next line
+    print()  # Move to the next line
 
 
-# # Number of rows for the diamond (top half)
-# num_rows = int(input("Enter the number of rows for the diamond: "))
+# Number of rows for the diamond (top half)
+num_rows = int(input("Enter the number of rows for the diamond: "))
 
-# # Print the top half of the diamond
-# for i in range(num_rows):
-#     # Print leading spaces
-#     print(' ' * (num_rows - i - 1), end='')
+# Print the top half of the diamond
+for i in range(num_rows):
+    # Print leading spaces
+    print(' ' * (num_rows - i - 1), end='')
     
-#     # Print the border of the top half of the diamond
-#     for j in range(2 * i + 1):
-#         if j == 0 or j == 2 * i or i == num_rows - 1:
-#             print('*', end='')
-#         else:
-#             print(' ', end='')
+    # Print the border of the top half of the diamond
+    for j in range(2 * i + 1):
+        if j == 0 or j == 2 * i or i == num_rows - 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
     
-#     print()  # Move to the next line
+    print()  # Move to the next line
 
-# # Print the bottom half of the diamond
-# for i in range(num_rows - 2, -1, -1):
-#     # Print leading spaces
-#     print(' ' * (num_rows - i - 1), end='')
+# Print the bottom half of the diamond
+for i in range(num_rows - 2, -1, -1):
+    # Print leading spaces
+    print(' ' * (num_rows - i - 1), end='')
     
-#     # Print the border of the bottom half of the diamond
-#     for j in range(2 * i + 1):
-#         if j == 0 or j == 2 * i or i == 0:
-#             print('*', end='')
-#         else:
-#             print(' ', end='')
+    # Print the border of the bottom half of the diamond
+    for j in range(2 * i + 1):
+        if j == 0 or j == 2 * i or i == 0:
+            print('*', end='')
+        else:
+            print(' ', end='')
     
-#     print()  # Move to the next line
+    print()  # Move to the next line
 
 Number_List = [1,2,3,4,5,6,7,8,9,10]
 Item_List = ["Apple","Banana","Cherry","Date","Elderberry","Fig","Grape","Honeydew","Indian Plum","Jackfruit"]
--- Unit 1/Operators.py
+++ Unit 1/Operators.py
@@ -1,27 +1,48 @@
-# Arithmetic Opertors in Python
-
+# Input numbers from the user
 num_1 = int(input("Enter the First Number : "))
 numb_2 = int(input("Enter the Second Number : "))
 
-add = num_1 + numb_2
-sub = num_1 - numb_2
-mul = num_1 * numb_2
-div = num_1 / numb_2
-f_div = num_1 // numb_2
-mod = num_1 % numb_2
-exp = num_1 ** numb_2
+# Arithmetic Operators
+print("\nArithmetic Operations:")
+print("Addition (+) :", num_1 + numb_2)
+print("Subtraction (-) :", num_1 - numb_2)
+print("Multiplication (*) :", num_1 * numb_2)
+print("Division (/) :", num_1 / numb_2)
+print("Floor Division (//) :", num_1 // numb_2)
+print("Modulus (%) :", num_1 % numb_2)
+print("Exponentiation (**) :", num_1 ** numb_2)
 
-print("Addition of Two Numbers :",add)
-print("Subtraction of Two Numbers :",sub)
-print("Multiplication of Two Numbers :",mul)
-print("Division of Two Numbers :",div)
-print("Floor Division of Two Numbers :",f_div)
-print("Modulus of Two Numbers :",mod)
-print("Exponential of Two Numbers :",exp)
+# Relational Operators
+print("\nRelational (Comparison) Operations:")
+print("Is num_1 greater than numb_2? (>) :", num_1 > numb_2)
+print("Is num_1 less than numb_2? (<) :", num_1 < numb_2)
+print("Is num_1 equal to numb_2? (==) :", num_1 == numb_2)
+print("Is num_1 not equal to numb_2? (!=) :", num_1 != numb_2)
+print("Is num_1 greater than or equal to numb_2? (>=) :", num_1 >= numb_2)
+print("Is num_1 less than or equal to numb_2? (<=) :", num_1 <= numb_2)
 
-# Relational Operators in python
+# Logical Operators
+print("\nLogical Operations:")
+print("Is num_1 > 0 AND numb_2 > 0? (and) :", num_1 > 0 and numb_2 > 0)
+print("Is num_1 > 0 OR numb_2 > 0? (or) :", num_1 > 0 or numb_2 > 0)
+print("NOT(num_1 < 0)? (not) :", not(num_1 < 0))
 
-# Logical Operator in python 
+# Bitwise Operators
+print("\nBitwise Operations:")
+print("Bitwise AND (&) :", num_1 & numb_2)
+print("Bitwise OR (|) :", num_1 | numb_2)
+print("Bitwise XOR (^) :", num_1 ^ numb_2)
+print("Bitwise NOT (~) of num_1 :", ~num_1)
+print("Bitwise Left Shift (<<) :", num_1 << 1)  # Shifts bits of num_1 to the left by 1
+print("Bitwise Right Shift (>>) :", num_1 >> 1)  # Shifts bits of num_1 to the right by 1
 
-# Membership Operator in python 
+# Membership Operators
+num_list = [1, 2, 3, 4, 5]
+print("\nMembership Operations:")
+print("Is num_1 in the list? (in) :", num_1 in num_list)
+print("Is numb_2 not in the list? (not in) :", numb_2 not in num_list)
 
+# Identity Operators
+print("\nIdentity Operations:")
+print("Is num_1 the same object as numb_2? (is) :", num_1 is numb_2)
+print("Is num_1 not the same object as numb_2? (is not) :", num_1 is not numb_2)
