22997f73b64e98059a0cfcb26536b1d01b0e41f8
mirajsankdecha <mirajsankdecha@gmail.com>
2024-11-25 13:18:58 +0530

    Basic of Numpy Updated

---
+++ Data Visualization using Python/Unit 1/basic_of_numpy.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+list1 = [1,2,3,4,5]
+array1 = np.array(list1)
+print(array1)
+
+list2 = [6,7,8,9,10]
+array2 = np.array(list2)
+print(array2)
+
+array3 = np.zeros(4)
+print(array3)
+
+array4 = np.arange(10)
+print(array4)
+
+array5 = np.arange(1,9,2)
+print(array5)
+
+array6 = np.floor(np.random.rand(5) * 10)
+print(array6)
+
+array7 = np.empty(4)
+print(array7)
+
+array8 = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print(array8)
+
