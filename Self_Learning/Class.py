94ea520919c7f3b90080d0129854902c1b37fd42
mirajsankdecha <mirajsankdecha@gmail.com>
2024-12-11 17:44:29 +0530

    Class in Python and Constructors

--- Self_Learning/Class.py
+++ Self_Learning/Class.py
@@ -1,1 +1,38 @@
+# Creating the Class
+class Animal :
+    def cow(self) :
+        print("Ohhhhhoooo")
+    def elephant(self) :
+        print("hmmmmmmmmmm")
+animal = Animal()
+output = animal.cow()
+print(f"Its Sounds like {output}")
+
+
+#  Creating the Constructor
+
+class Calculation :
+    def __init__(self,X,Y) :
+        self.x = X
+        self.y = Y
+        sum = X + Y
+        print(f"Sum of Two Number is : {sum}")
+    def sub(self) :
+        result = self.x - self.y
+        print(f"Substraction of Two Number is : {result}")  
+    def mul(self) :
+        result = self.x * self.y
+        print(f"Multiplication of Two Number is : {result}")
+    def div(self) :
+        result = self.x / self.y
+        print(f"Division of Two Numbers is : {result}")
+    def mod(self) :
+        result = self.x % self.y
+        print(f"The Modulo of Two Number is : {result}")
+                   
+cal = Calculation(X = 10, Y = 10)
+cal.sub()
+cal.mul()
+cal.div()
+cal.mod()
 
