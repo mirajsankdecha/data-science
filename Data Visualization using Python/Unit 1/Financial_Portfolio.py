77720b4a64a75018e89a293a0c80f8ebf9ddb7aa
mirajsankdecha <mirajsankdecha@gmail.com>
2024-12-12 12:32:37 +0530

    Task 7 Compelete

--- Data Visualization using Python/Unit 1/Financial_Portfolio.py
+++ Data Visualization using Python/Unit 1/Financial_Portfolio.py
@@ -18,6 +18,14 @@ print(f"The Price of 3rd Company on 4th Day is : {access_price}")
 slice_price = stock_price[:2,:3]
 print(f"Price of first two company on 3 days is :\n {slice_price}")
 
-#Takk 5 : Find the price of company that are greater than 300 
+#Task 5 : Find the price of company that are greater than 300 
 greater_price = stock_price[stock_price > 300]
 print(f"The company which price have greater than 300 are :\n {greater_price}")
+
+#Task 6 : Calculate the daily return for first company
+daily_returns_company1 = (stock_price[0,1:] - stock_price[0,:-1]) / stock_price[0,:-1]
+print(f"Daily Return of 1st company is :\n {daily_returns_company1}")
+
+#Task 7 : Find the sum of all stock price
+sum_price = stock_price.sum()
+print(f"Sum of Price of Compnies is : {sum_price}")
