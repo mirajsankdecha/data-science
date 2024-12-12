51a50a2bee2799c309382f35b8d2c2c9c57a1e93
mirajsankdecha <mirajsankdecha@gmail.com>
2024-12-12 12:06:51 +0530

    Perform Another Task

--- Data Visualization using Python/Unit 1/Financial_Portfolio.py
+++ Data Visualization using Python/Unit 1/Financial_Portfolio.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # Task 1 : Create a Array
 stock_price = np.random.randint(100,500, size=((5,7)))
-print(f"Price of Company : {stock_price}")
+print(f"Price of Company :\n {stock_price}")
 
 #Task 2 : Find the Shape of Array
 shape = stock_price.shape
@@ -14,3 +14,10 @@ access_price = stock_price[2,3]
 print(f"The Price of 3rd Company on 4th Day is : {access_price}")
 
 #Task 4 : Slice the price of first two company for first three days
+
+slice_price = stock_price[:2,:3]
+print(f"Price of first two company on 3 days is :\n {slice_price}")
+
+#Takk 5 : Find the price of company that are greater than 300 
+greater_price = stock_price[stock_price > 300]
+print(f"The company which price have greater than 300 are :\n {greater_price}")
