c3a8d14ec38be2c067a3a60d45bb505bc55f61c1
mirajsankdecha <mirajsankdecha@gmail.com>
2024-12-13 16:54:52 +0530

    all task compelete

--- Data Visualization using Python/Unit 1/Financial_Portfolio.py
+++ Data Visualization using Python/Unit 1/Financial_Portfolio.py
@@ -73,3 +73,18 @@ array1, array2 = stock_price[:3], stock_price[3:]
 print(f"Array 1 :\n {array1}")
 print(f"Array 2 :\n {array2}")
 
+#Task 18 : Calculate the cumulative return for each company :
+daily_return = np.diff(stock_price, axis=1) / stock_price[:,:-1]
+cumulative_return = (np.prod(1 + daily_return, axis=1) -1) * 100
+print(f"Cumulative return for each company :\n {cumulative_return}")
+
+#Task 19 : Identify days where all stocks prices are above 100
+above_100 = np.all(stock_price > 100, axis=0)
+print(f"Days where all stock prices are above 100 :\n {above_100}")
+
+# Calculate the ratio of mean return to standard deviation
+mean_returns = daily_return.mean(axis=1)
+std_returns = daily_return.std(axis=1)
+mean_std_ratio = mean_returns / std_returns
+print(f"Ratio of mean return to standard devaition : {mean_std_ratio}")
+
