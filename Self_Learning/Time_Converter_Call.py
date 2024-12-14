baa389c75d2989cd20eb08bef9c43c63c14c48fc
mirajsankdecha <mirajsankdecha@gmail.com>
2024-12-15 01:38:47 +0530

    Module Importing

---
+++ Self_Learning/Time_Converter_Call.py
@@ -0,0 +1,31 @@
+from TimeConverter import TimeConverter
+
+def main() : 
+    print("Welcome to the time program")
+    value = float(input("Enter the value to convert > "))
+    converter = Time_Converter(value)
+    
+    print("\nChoose the options : ")
+    print("1. Seconds to Minutes")
+    print("2. Seconds to Hours")
+    print("3. Minutes to Seconds")
+    print("4. Hours to Seconds")
+    print("5. Hours to Minutes")
+    
+    choice = int(input("Enter your choice from given options > "))
+    
+    if choice == 1 :
+        print(f"{value} second = {converter.seconds_to_minutes()} minutes")
+    elif choice == 2:
+        print(f"{value} seconds = {converter.seconds_to_hours()} hours")
+    elif choice == 3:
+        print(f"{value} minutes = {converter.minutes_to_seconds()} seconds")
+    elif choice == 4:
+        print(f"{value} hours = {converter.hours_to_seconds()} seconds")
+    elif choice == 5:
+        print(f"{value} hours = {converter.hours_to_minutes()} minutes")
+    else:
+        print("Invalid choice!")
+        
+if __name__ == "__main__" :
+    main()            
