2c8b11c5b4e39d56f0bf1decdf3b8cb23aed4a9a
mirajsankdecha <mirajsankdecha@gmail.com>
2024-12-16 11:35:27 +0530

    Functions of pandas

--- Data Visualization using Python/Unit 2/basic_of_pandas.py
+++ Data Visualization using Python/Unit 2/basic_of_pandas.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
-# Print from CSV file
-states = pd.read_csv("E:/python/Data Visualization using Python/Unit 2/states.csv")
-print(states)
+#Print from CSV file
+# states = pd.read_csv("E:/python/Data Visualization using Python/Unit 2/states.csv")
+# print(states)
 
 #Print from Excel file
 kc = pd.read_csv("E:/python/Data Visualization using Python/Unit 2/kc_house_data.csv")
-print(kc)
+print(kc) # Print first and last 5 rows and columns
+print(kc.head()) # print the first 5 rows
+print(type(kc)) # Print the type of dataframe
+print(kc.columns) # Print all the columns
+print(f"Total Rows in dataset : {len(kc)}")
+print(kc.shape)
+print(kc.size)
