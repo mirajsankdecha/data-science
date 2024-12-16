d304ccbdc5f7e388b5ca75570709d371ca5b53c5
mirajsankdecha <mirajsankdecha@gmail.com>
2024-12-16 12:29:30 +0530

    Array to Dataframe

--- Data Visualization using Python/Unit 2/pandas_from_list.py
+++ Data Visualization using Python/Unit 2/pandas_from_list.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 # Converting the list to series
 data = [10,20,30,40,50]
 my_series = pd.Series(data)
@@ -9,6 +10,7 @@ s = pd.Series(new_data, index=["x","y","z"])
 print(f"Filterout using the lables :\n {s} ")
 
 # Converting the dictonary to series
+# It work on 1D array
 dic = {
     "Name" : "Miraj Sankdecha",
     "Age" : 21,
@@ -16,3 +18,17 @@ dic = {
 }
 s = pd.Series(dic)
 print(f"Print the converted dic : \n{s}")
+# Its works on rows and columns if we have 2D array
+stu_nm = {
+    "Students" : ["Darshit","Tanay","Kapil"]
+}
+ps = pd.DataFrame(stu_nm)
+print(ps)
+
+# Convert the 2D array into dataframe
+
+info_data = [["Miraj",21,"Rajkot"],
+             ["Vrushti",20,"Jalgaov"]]
+df = pd.DataFrame(info_data,columns=["Name","Age","City"])
+print(df)
+
