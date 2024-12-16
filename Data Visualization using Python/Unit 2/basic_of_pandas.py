dcfbebbf1c5c75b8ab4b507a96d452adc2b67e98
mirajsankdecha <mirajsankdecha@gmail.com>
2024-12-16 11:27:03 +0530

    Basics of python started

---
+++ Data Visualization using Python/Unit 2/basic_of_pandas.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Print from CSV file
+states = pd.read_csv("E:/python/Data Visualization using Python/Unit 2/states.csv")
+print(states)
+
+#Print from Excel file
+kc = pd.read_csv("E:/python/Data Visualization using Python/Unit 2/kc_house_data.csv")
+print(kc)
