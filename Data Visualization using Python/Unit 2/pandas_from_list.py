5215de348ed1d6c1a11351e48607fe2ef87ae4d1
mirajsankdecha <mirajsankdecha@gmail.com>
2024-12-16 12:11:57 +0530

    Converting the list and dic to series using pandas

--- Data Visualization using Python/Unit 2/pandas_from_list.py
+++ Data Visualization using Python/Unit 2/pandas_from_list.py
@@ -1,5 +1,18 @@
 import pandas as pd
-
+# Converting the list to series
 data = [10,20,30,40,50]
 my_series = pd.Series(data)
 print(f"The Series of list is : {my_series}")
+print(f"Filterout the values by indexing : {my_series[2]}")
+new_data = [1,2,3]
+s = pd.Series(new_data, index=["x","y","z"])
+print(f"Filterout using the lables :\n {s} ")
+
+# Converting the dictonary to series
+dic = {
+    "Name" : "Miraj Sankdecha",
+    "Age" : 21,
+    "Gender" : "Male"
+}
+s = pd.Series(dic)
+print(f"Print the converted dic : \n{s}")
