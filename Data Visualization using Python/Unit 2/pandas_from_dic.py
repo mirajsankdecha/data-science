2ca1a74b59a1cdaf62fc0146e91b7398537fbf55
mirajsankdecha <mirajsankdecha@gmail.com>
2024-12-19 11:35:58 +0530

    Rename the index

--- Data Visualization using Python/Unit 2/pandas_from_dic.py
+++ Data Visualization using Python/Unit 2/pandas_from_dic.py
@@ -6,6 +6,14 @@ data = {
     'City' : ["Rajkot","Rajkot","Babra","Babra"]
 }
 df = pd.DataFrame(data)
-df.set_index("Name",inplace=False) # It will do the modification in the object
+df.set_index("Name",inplace=False) # It will do the modification in the object(inplace=)
 print(df)
+
+
+# Rename the index
+df.rename(index={0:'A',1:'B',2:'C'}, inplace=True) # When we use false we get the new object with the modification while the leaving the orginal object unchanged
+# Display dataframe after index is renamed
+print(f"Modified Dataframe : {df}") # No new object is return 
+
+
 
