179ea1bd3173aeb78b15537f3413c574fa2d5dd1
mirajsankdecha <mirajsankdecha@gmail.com>
2024-12-19 12:31:43 +0530

    Use the Statistical Method

--- Data Visualization using Python/Unit 2/work_with_kc_data.py
+++ Data Visualization using Python/Unit 2/work_with_kc_data.py
@@ -7,3 +7,7 @@ print(f"Sum of Value is :\n {house.sum()}") # Get the sum value from dataset
 print(f"Filter the Numeric Value is :\n {house.sum(numeric_only=True)}") 
 print(f"Count of Value :\n {house.count()} ")
 print(f"Filter the DataFrame is :\n {house.tail(52).head(51).sum(numeric_only=True)}")
+print(house.mean(numeric_only=True))
+print(house.median(numeric_only=True))
+print(house.mode(numeric_only=True))
+print(f"Analysis of Dataframe is :\n {house.describe()}") # Best function for Statistial Analysis
