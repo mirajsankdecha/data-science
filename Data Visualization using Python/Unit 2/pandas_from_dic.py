2bb6b525369ad95aa3d32730f9df5c606173ec3a
mirajsankdecha <mirajsankdecha@gmail.com>
2024-12-19 11:43:29 +0530

    Creating the RangeIndex

--- Data Visualization using Python/Unit 2/pandas_from_dic.py
+++ Data Visualization using Python/Unit 2/pandas_from_dic.py
@@ -1,19 +1,24 @@
 import pandas as pd
 
+# Original data
 data = {
-    'Name' : ["Miraj","Janvi","Darshit","Tanay"],
-    'Age'   : [21,20,21,21],
-    'City' : ["Rajkot","Rajkot","Babra","Babra"]
+    'Name': ["Miraj", "Janvi", "Darshit", "Tanay"],
+    'Age': [21, 20, 21, 21],
+    'City': ["Rajkot", "Rajkot", "Babra", "Babra"]
 }
 df = pd.DataFrame(data)
-df.set_index("Name",inplace=False) # It will do the modification in the object(inplace=)
-print(df)
 
+# Example 1: Using set_index without inplace=False
+df_with_name_index = df.set_index("Name")  # Returns a new object
+print("DataFrame with 'Name' as index:")
+print(df_with_name_index)
 
-# Rename the index
-df.rename(index={0:'A',1:'B',2:'C'}, inplace=True) # When we use false we get the new object with the modification while the leaving the orginal object unchanged
-# Display dataframe after index is renamed
-print(f"Modified Dataframe : {df}") # No new object is return 
+# Example 2: Creating a DataFrame with RangeIndex
+df_with_range_index = pd.DataFrame(data, index=pd.RangeIndex(5, 9, name='index'))  # Correct range to match data length
+print("\nDataFrame with RangeIndex:")
+print(df_with_range_index)
 
-
-
+# Rename the index of the DataFrame with RangeIndex
+df_with_range_index.rename(index={5: 'A', 6: 'B', 7: 'C', 8: 'D'}, inplace=True)  # Correct keys
+print("\nModified DataFrame after renaming index:")
+print(df_with_range_index)
