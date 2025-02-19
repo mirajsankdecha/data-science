30419544d6c368505bcd8794188e6bff742b484b
mirajsankdecha <mirajsankdecha@gmail.com>
2025-02-19 15:21:37 +0530

    Task of python

--- Self_Learning/Pattern.py
+++ Self_Learning/Pattern.py
@@ -5,14 +5,19 @@
 #     print("*" * i)
 
 
-for i in range(1, 8 ,1):
-    print("*" * i)
-for i in range(6, 0, -1):
-    print("*" * i)
+# for i in range(1, 8 ,1):
+#     print("*" * i)
+# for i in range(6, 0, -1):
+#     print("*" * i)
 
 
-test = input("Enter the test : ")
-test = test.replace("2", "@")
-test = test.replace("3", "$")
-test = test.replace("4", "#")
-print(test)
+# test = input("Enter the test : ")
+# test = test.replace("2", "@")
+# test = test.replace("3", "$")
+# test = test.replace("4", "#")
+# print(test)
+
+for i in range(0,11,1):
+    for j in range(1, 11, 1):
+        print("*" * j, end="")
+    print()
---
+++ Self_Learning/Task_1.py
@@ -0,0 +1,29 @@
+# # write a easy program of palidrome of string wothout including space
+# text = input("Enter the text: ")
+# text = text.lower().replace(" ", "")
+# if text == text[::-1]:
+#     print("The text is palidrome")
+# else:
+#     print("The text is not palidrome")
+
+# # Task 2 : Print the list of squares even number only
+# list1 = [1,2,3,4,5,6,7,8,9,10]
+# list2 = []
+# for i in list1:
+#     if i % 2 == 0:
+#         list2.append(i**2)
+# print(list2)
+
+# Task 3 : Create and Store the name and age of 5 students in dictionary and find a student exits or not
+students = {}
+for i in range(5):
+    name = input("Enter the name of student: ")
+    age = int(input("Enter the age of student: "))
+    students[name] = age
+print(students)
+name = input("Enter the name of student to check: ")
+if name in students:
+    print("The student exits")
+else:
+    print("The student does not exits")
+
